c1d7f177b60b61f223c2c81e39795313b92285f0
Mascode <thomas.jego@protonmail.com>
2025-08-29 14:53:18 +0200

    Create bot.py + Setup python env

---
+++ bot.py
@@ -0,0 +1,24 @@
+﻿import os
+import dotenv
+import discord
+from discord.ext import commands
+
+dotenv.load_dotenv()
+
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Bot initialisé.")
+    try:
+        synced = await client.tree.sync()
+        print(f"✓ Commandes synchronisées: {len(synced)}")
+    except Exception as e:
+        print(f"✗ Échec de la synchronisation des commandes: {e}")
+
+
+@client.tree.command(name="event", description="Return the last Notion event")
+async def event(interaction: discord.Interaction):
+    await interaction.response.send_message("Voici le dernier événement Notion.")
+
+client.run(os.getenv("DISCORD_TOKEN"))
